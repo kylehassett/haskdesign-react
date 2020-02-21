cc39c63febf6f81a8ab2efc6adbad0455510935f
kylehassett <hassettkyle@gmail.com>
2020-02-21 13:08:45 -0500

    Setting up an "underconstruction" landing page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hi! Welcome to haskdesign.
+        </p>
+        <p>The site is currently under construction. Thanks for visiting!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
