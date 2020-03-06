7c4d664035c9b02bb26444acfe3b9e0072e1da64
kylehassett <hassettkyle@gmail.com>
2020-03-05 22:58:26 -0500

    added structure to header and added light mode / dark mode toggle

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,23 +1,57 @@
+/* @jsx jsx */
+import { jsx } from '@emotion/core';
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Switch from 'react-switch';
+
+import { setUserInterfaceMode, userInterfaceMode } from '../../../actions';
 
 
-function Header() {
+function Header(props) {
+  const handleCheckChanged = (checked) => {
+    props.setUserInterfaceMode(checked ? userInterfaceMode.DARK_MODE : userInterfaceMode.LIGHT_MODE);
+  };
+
+  const renderLink = (href, title) => {
+    return (
+      <div className="col" align="center">
+        <Link to={href}>{title}</Link>
+      </div>
+    );
+  };
+
   return (
-    <div className="container">
+    <div className="container" align="center">
       <div className="row">
-        <div className="col">
-          <Link to={"/"}>HaskDesign</Link>
+        <div className="col-8 offset-md-2" align="center">
+          <h1>Hask Design</h1>
         </div>
-        <div className="col">
-          <Link to={"/work"}>Work</Link>
+        <div className="col-md-2">
+          <p css={{ display: "inline" }}>Dark Mode: </p>
+          <Switch
+            checked={props.userInterfaceMode === userInterfaceMode.DARK_MODE}
+            onChange={handleCheckChanged}
+          />
         </div>
-        <div className="col">
-          <Link to={"/resume"}>Resume</Link>
-        </div>
+      </div>
+      <div className="row">
+        { renderLink("/", "Home") }
+        { renderLink("/work", "Work") }
+        { renderLink("/resume", "Resume") }
       </div>
     </div>
   );
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    userInterfaceMode: state.userInterface.userInterfaceMode
+  };
+}
+
+const mapDispatchToProps = {
+  setUserInterfaceMode
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
