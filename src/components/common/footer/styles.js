33e3cf62da4d19b0111f10f11f90835c2a66f497
kylehassett <hassettkyle@gmail.com>
2020-04-08 07:29:30 -0400

    added the picture of me

--- src/components/common/footer/styles.js
+++ src/components/common/footer/styles.js
@@ -1,12 +1,6 @@
 export const getFooterStyles = (theme) => ({
   backgroundColor: theme.colors.background,
   fontSize: 18,
-  topLink: {
-    marginTop: theme.whiteSpace.md,
-  },
-  bottomLink: {
-    marginBottom: theme.whiteSpace.md
-  },
   a: {
     color: theme.colors.ink.forty
   },
--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -7,21 +7,28 @@ import { getHomeStyles } from './styles';
 
 function Home() {
   return (
-    <div className="container.fluid h-100 d-flex flex-column" css={getHomeStyles}>
-      <div className="row d-flex" align="center">
-        <div className="col-6 d-flex justify-content-end align-items-end">
-          <p>HAS</p>
+    <div className="w-100 h-100 d-flex flex-row" css={getHomeStyles}>
+      <div className="container.fluid h-100 d-flex flex-column flex-fill">
+        <div className="row d-flex" align="center">
+          <div className="col d-flex justify-content-end align-items-end">
+            <p>HAS</p>
+          </div>
+        </div>
+        <div className="row d-flex" align="center">
+          <div className="col d-flex justify-content-center align-items-center">
+            <p>KDES</p>
+          </div>
+        </div>
+        <div className="row d-flex" align="center">
+          <div className="col d-flex justify-content-end align-items-start">
+            <p>IGN</p>
+          </div>
         </div>
       </div>
-      <div className="row d-flex" align="center">
-        <div className="col-6 d-flex justify-content-center align-items-center">
-          <p>KDES</p>
-        </div>
-      </div>
-      <div className="row d-flex" align="center">
-        <div className="col-6 d-flex justify-content-end align-items-start">
-          <p>IGN</p>
-        </div>
+      <div className="d-flex flex-column justify-content-center h-100 ml-3">
+        <figure>
+          <img src="../../MyFaceCutout.png" className="img-fluid h-100" alt="My face cutout"/>
+        </figure>
       </div>
     </div>
   );
